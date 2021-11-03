a63bf3df4da12b2d22704cd98a3ae4ffa2206f7f
Paul Woodgate <paul.woodgate@gmail.com>
2021-11-03 23:09:09 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,14 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(xss());
 app.use(hpp());
-app.use(helmet.contentSecurityPolicy({ useDefaults: true }));
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      'script-src': ["'self'", 'mongodb.com']
+    }
+  })
+);
 app.use('/assets', express.static(path.join(__dirname, '../assets')));
 app.use(express.static(path.resolve(__dirname, '../dist'), { maxAge: '1y', etag: false }));
 
